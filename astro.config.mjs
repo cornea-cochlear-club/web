b2b4c62ec849722282f84735f2610a256af3c7c2
Wasawat Somno <wasawat.som@cjexpress.co.th>
2024-05-30 18:43:44 +0700

    update deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import react from "@astrojs/react"
 
 // https://astro.build/config
-export default defineConfig({});
+import vercel from "@astrojs/vercel/serverless"
+
+// https://astro.build/config
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), react()],
+  output: "server",
+  adapter: vercel(),
+})
