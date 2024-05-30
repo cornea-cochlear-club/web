663d81e8f89f6d222a9796e85cb3a685a8f7ce65
Karnpapon Boonput <karnpapon@gmail.com>
2024-05-30 21:53:08 +0700

    add Phosphene font & update landing page

--- src/layouts/threeLayout.jsx
+++ src/layouts/threeLayout.jsx
@@ -8,5 +8,36 @@ export const render = () =>
       <Canvas>
         <Scene />
       </Canvas>
+      <div className="absolute inset-0">
+        <div className="flex flex-col relative w-full h-full items-center">
+          <p className="flex text-white  font-display text-2xl">Cornea Cochlear Club</p>
+          <p className="">present</p>
+          <div className="flex flex-col justify-center items-center">
+            <p className="relative flex font-mono mt-2">Live-Coding Audio/Visual</p>
+            <div className="flex justify-center items-center my-4">
+              <p className="relative flex text-white font-display text-[6rem]">Cybernaut Party</p>
+              <p className="absolute  text-[#00ff00] font-mono text-[5rem] inline-block scale-y-[2]">CYBERNAUT PARTY</p>
+            </div>
+            <p className="relative flex text-[#00ff00] font-mono -top-[1rem] scale-x-[1.8] scale-y-[2.9]">THE STREET FINDS ITS OWN USES FOR THINGS</p>
+            <ol className="flex flex-col justify-center items-center space-y-2 font-mono mt-4 text-[#ff006a] leading-tight text-xl">
+              <li>kastakila (NYC)</li>
+              <li>Reckoner (NYC)</li>
+              <li>emptyflash (NYC)</li>
+              <li>Renick Bell (HCMC)</li>
+              <li>Le-Bac-Tan (HCMC)</li>
+              <li>inmintcondition (BKK)</li>
+              <li>Varut_O (BKK)</li>
+              <li>Palindroma (BKK, RU)</li>
+              <li>WrappedByte (BKK)</li>
+              <li>CRSRCRSR (BKK)</li>
+              <li>Karnpapon Boonput (Former "The ███████", BKK)</li>
+            </ol>
+
+            <div>
+              {/* sponsor logos */}
+            </div>
+          </div>
+        </div>
+        </div>
     </>
   )
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,6 +10,7 @@ module.exports = {
       fontFamily: {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
         mono: ["JetBrains Mono"],
+        display: [ "Phosphene" ],
       },
     },
     colors: {
