df6533590dd4a48f2c275b3d6295f6f290014b27
Wasawat Somno <wasawat.som@cjexpress.co.th>
2024-06-12 23:49:32 +0700

    add full sponsors

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,6 +5,8 @@ import Pwin from "../../public/sponsors/pwin.png"
 import SquareDot from "../../public/sponsors/SquareDots-Logo-Final_White-1.svg"
 import Synap from "../../public/sponsors/synap-logo-1.svg"
 import Unformat from "../../public/sponsors/unformat-logo-transparent-1.svg"
+import Cleverse from "../../public/sponsors/cleverse.png"
+import DigitalBoyz from "../../public/sponsors/dgboyz.png"
 
 const Footer = () => {
   return (
@@ -19,12 +21,16 @@ const Footer = () => {
         </a>
       </div>
 
-      <div class="flex space-x-12">
+      <div class="flex space-x-6">
         {/* sponsor logos */}
-        <img src={Synap.src} class="w-[45px] mr-2" />
-        <img src={Unformat.src} class="w-[60px] mr-2" />
-        <img src={SquareDot.src} class="w-[45px] mr-2" />
-        <img src={Pwin.src} class="w-[40px] mr-2" />
+        <img src={Synap.src} class="w-[45px]" />
+        <img src={Unformat.src} class="w-[60px]" />
+        <img src={SquareDot.src} class="w-[45px]" />
+        <img src={Pwin.src} class="w-[40px]" />
+        <div class="flex space-x-2">
+          <img src={Cleverse.src} class="w-[80px] object-contain" />
+          <img src={DigitalBoyz.src} class="w-[100px] object-contain" />
+        </div>
       </div>
     </div>
   )
